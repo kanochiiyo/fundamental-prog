3aa7d20b6b59388435ec2f6c0997fb8ae8393deb
Andini Andaresta <133855037+kanochiiyo@users.noreply.github.com>
2024-06-27 09:49:48 +0000

    committed

---
+++ struktur-data/main.py
@@ -0,0 +1,10 @@
+# Struktur Data => cara mengorganisir dan menyimpan data dalam komputer agar dapat diakses dan dimanipulasi dengan mudah
+# YANG WAJIB DIKUASAI => (1) berpikir sistematis, problem solving, efisiensi
+
+# Struktur Data :
+# Array = tipe data sama
+# Linked list = punya head dan tail (mirip gerbong kereta)
+# Stack = Tumpukan last in first out
+# Queue = Antrian first in first out
+# Tree = struktur hierarkis dengan node dan punya cabang (anak)
+# Graph = terdiri dari titik-titik yang terhubung 
